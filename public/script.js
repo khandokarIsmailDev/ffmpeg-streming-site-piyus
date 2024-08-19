e872413c89fb22f903a461c0e0bb5186bf338927
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-08-19 13:22:17 +0600

    now ready , refresh the see the live video will see

---
+++ public/script.js
@@ -0,0 +1,6 @@
+const userVideo = document.getElementById('user-video')
+
+window.addEventListener('load',async(event)=>{
+    const media = await navigator.mediaDevices.getUserMedia({audio:true,video:true})
+    userVideo.srcObject = media
+})
